8e0a76d63e0ea6991907cd02d30f9ba27446361d
Sameer Chand <sam@manishas-MacBook-Air.local>
2024-06-14 13:25:38 +0530

    added search functionality

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -12,27 +12,50 @@ const CountryCard = ({ name, flag, alt }) => {
 
 export default function Flags() {
   const [countries, SetCountries] = useState([]);
+  const [onMount, setOnMount] = useState(false);
 
+  const [flagsToRender, setFlagsToRender] = useState([]);
+  const [filter, setFilter] = useState("");
   useEffect(() => {
     try {
       axios("https://restcountries.com/v3.1/all").then((res) =>
         SetCountries(res.data)
       );
+      setOnMount(true);
     } catch (e) {
       console.log(e);
     }
   }, []);
+  useEffect(() => {
+    if (filter === "") {
+      setFlagsToRender([...countries]);
+    } else {
+      setFlagsToRender(
+        countries.filter((country) => country.name.common.includes(filter))
+      );
+      console.log(flagsToRender);
+    }
+  }, [onMount, filter]);
 
   return (
-    <div className="container">
-      {countries.map((country) => (
-        <CountryCard
-          key={country.name.official}
-          name={country.name.common}
-          flag={country.flags.png}
-          alt={country.flags.alt}
-        />
-      ))}
+    <div className="parent">
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value);
+        }}
+        className="inputField"
+      />
+      <div className="container">
+        {flagsToRender.map((country) => (
+          <CountryCard
+            key={country.name.official}
+            name={country.name.common}
+            flag={country.flags.png}
+            alt={country.flags.alt}
+          />
+        ))}
+      </div>
     </div>
   );
 }
